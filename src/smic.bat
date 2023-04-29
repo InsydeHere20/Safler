7ab32d448fc21fe3748f6c82cc9bfdfde60d45ba
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2023-04-29 22:25:35 +0200

    icaddScriprqz zorgk

--- src/smic.bat
+++ src/smic.bat
@@ -1,5 +1,14 @@
 @echo off
 
 for i in i {
-    echo "Welcome to Niosem SAFLER"
+    echo "Welcome to Niosem SAFLER's Special Mixed Integration Control (SMIC)"
+    echo ""
+    echo "==================================================================="
+    echo "|              Integration Control version 0.0.1                  |"
+    echo "==================================================================="
+    choice "$choice"
+    if $choice = 1 do {
+        powershell
+        iex "github.com/Niosem/Safler/IntContr-Opr/icaddscript.ps1"
+    }
 }
